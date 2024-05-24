89f056414b49bfa761472c87489b6f6fd9b7fc78
yoshini08 <155824588+yoshini08@users.noreply.github.com>
2024-05-24 13:10:53 +0530

    Create Double linkedlist construction - DOUBLE LL.py

---
+++ Double linkedlist construction - DOUBLE LL.py
@@ -0,0 +1,48 @@
+class Box:
+   def __init__(self, data):
+       self.data = data
+       self.prev = None
+       self.next = None
+
+
+      
+def findTail(head):
+   if head == None:
+       return None
+   tail = head
+   while tail.next != None:
+       tail = tail.next
+   return tail
+
+
+def insertAtEnd(head, ele):
+   newNode = Box(ele)
+   if head == None:
+       return newNode
+   tail = findTail(head)
+   tail.next = newNode
+   newNode.prev = tail 
+return head
+
+
+def printLeftToRight(head):
+   curr = head
+   while curr != None:
+       print(curr.data, end = " ")
+       curr = curr.next
+   print()
+  
+def printRightToLeft(head):
+   tail = findTail(head)
+   while tail != None:
+       print(tail.data, end = " ")
+       tail = tail.prev
+   print()
+      
+n = int(input())
+l = list(map(int, input().split()))
+head = None
+for ele in l:
+   head = insertAtEnd(head, ele)
+printLeftToRight(head)
+printRightToLeft(head)
